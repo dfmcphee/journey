eb856a5fd99984ab9cf63a655d89e35a31fb210a
Dominic McPhee <dominic.mcphee@gmail.com>
2017-10-15 15:13:21 -0400

    Updating place names and map zoom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import MapGL, {Marker, NavigationControl} from 'react-map-gl';
+import React, {Component} from 'react';
+import MapGL, {Marker} from 'react-map-gl';
+import {Motion, spring} from 'react-motion';
 
-import locations from './locations.json';
+import geography from './locations.json';
 import Pin from './Pin';
 
 import './App.css';
@@ -9,40 +10,88 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 const TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
-const navStyle = {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  padding: '10px'
-};
+function slugify(text) {
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w-]+/g, '')       // Remove all non-word chars
+    .replace(/--+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
     
+    const locations = geography.features
+      .map((location) => {
+        const dateString = Date.parse(location.properties.date);
+        if (!isNaN(dateString)) {
+          location.properties.date = new Date(dateString);
+        } else {
+          location.properties.date = false;
+        }
+        return location;
+      })
+      .filter((location) => location.properties.date);
+
+    locations.sort(function (a, b) {
+      return a.properties.date - b.properties.date;
+    });
+    
     this.state = {
       viewport: {
-        latitude: 53,
-        longitude: -2,
-        zoom: 5.5,
-        bearing: 0,
-        pitch: 0,
+        latitude: locations[0].geometry.coordinates[1] - 0.1,
+        longitude: locations[0].geometry.coordinates[0],
         width: 500,
-        height: 500,
-      }
+        height: 500
+      },
+      selectedWaypoint: 0,
+      locations
     };
   }
 
   componentDidMount() {
-    window.addEventListener('resize', this._resize);
-    this._resize();
+    window.addEventListener('keyup', this._handleKeypress);
+    window.addEventListener('resize', this._handleResize);
+    this._handleResize();
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this._resize);
+    window.removeEventListener('keyup', this._handleKeypress);
+    window.removeEventListener('resize', this._handleResize);
   }
 
-  _resize = () => {
+  _handleKeypress = (evt) => {
+    if (!(evt.code === 'ArrowRight' || evt.code === 'ArrowLeft')) {
+      return;
+    }
+
+    this.setState(({selectedWaypoint, locations, viewport}) => {
+      if (
+        (evt.code === 'ArrowLeft' && selectedWaypoint === 0) ||
+        (evt.code === 'ArrowRight' && selectedWaypoint === locations.length -1)
+      ) { return; }
+      
+      const newlySelected = (evt.code === 'ArrowLeft')
+        ? selectedWaypoint - 1
+        : selectedWaypoint + 1;
+
+      const location = locations[newlySelected];
+
+      return {
+        selectedWaypoint: newlySelected,
+        viewport: {
+          height: viewport.height,
+          width: viewport.width,
+          latitude: location.geometry.coordinates[1] - 0.1,
+          longitude: location.geometry.coordinates[0]
+        }
+      };
+    });
+  };
+
+  _handleResize = () => {
     this.setState({
       viewport: {
         ...this.state.viewport,
@@ -57,34 +106,57 @@ class App extends Component {
   }
 
   render() {
-    const {viewport} = this.state;
-    const markers = locations.features.map((location, i) => {
+    const {viewport, locations, selectedWaypoint} = this.state;
+    const markers = locations.map((location, i) => {
+      const pinSize = (selectedWaypoint === i) ? 32 : 20;
       return (
         <Marker
           key={`marker-${i}`}
           latitude={location.geometry.coordinates[1]}
           longitude={location.geometry.coordinates[0]}
         >
-          <Pin size={20} />
+          <Motion style={{
+            pinSize: spring(pinSize)
+          }}>
+            {({ pinSize }) => <Pin size={pinSize} />}
+          </Motion>
         </Marker>
       );
     });
 
+    const placeName = locations[selectedWaypoint] && locations[selectedWaypoint].properties.place_name
+      ? locations[selectedWaypoint].properties.place_name
+      : null;
+
+    const date = locations[selectedWaypoint] && locations[selectedWaypoint].properties.date
+      ? locations[selectedWaypoint].properties.date.toLocaleDateString('en-US')
+      : null;
+
     return (
       <div className="App">
-        <MapGL
-          {...viewport}
-          mapboxApiAccessToken={TOKEN}
-          onViewportChange={this._updateViewport}
-          mapStyle="mapbox://styles/mapbox/dark-v9"
-        >
-
-          {markers}
-
-          <div className="nav" style={navStyle}>
-            <NavigationControl onViewportChange={this._updateViewport} />
-          </div>
-        </MapGL>
+        <Motion style={{
+          latitude: spring(viewport.latitude),
+          longitude: spring(viewport.longitude),
+        }}>
+          {({ latitude, longitude }) => <MapGL
+            latitude={latitude}
+            longitude={longitude}
+            height={viewport.height}
+            width={viewport.width}
+            zoom={9}
+            bearing={0}
+            pitch={0}
+            mapboxApiAccessToken={TOKEN}
+            onViewportChange={this._updateViewport}
+            mapStyle="mapbox://styles/mapbox/dark-v9"
+          >
+            {markers}
+          </MapGL>}
+        </Motion>
+        <div className={`information-overlay information-overlay--${slugify(placeName)}`}>
+          <p className="information-overlay__date">{date}</p>
+          <p className="information-overlay__name">{placeName}</p>
+        </div>
       </div>
     );
   }
